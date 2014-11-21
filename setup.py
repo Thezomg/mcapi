e15175bbe61a927499b11bb8c97c398ad19eee6d
Deaygo <deaygo@thezomg.com>
2014-11-21 08:16:37 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mcapi',
-    version='0.2',
+    version='0.3',
     description='Simple mc api library',
     install_requires=['requests'],
     author='Deaygo',
